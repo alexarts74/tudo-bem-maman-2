dc6dd8be89bc1a49a18f6ae7364a582e5d6ded1d
alexandre artus <artusalexandre@yahoo.fr>
2023-04-13 17:02:35 +0200

    generation view devise et installation logo

--- app/controllers/clothes_controller.rb
+++ app/controllers/clothes_controller.rb
@@ -1,5 +1,5 @@
 class ClothesController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[index show]
+  skip_before_action :authenticate_user!, only: %i[home index show]
 
   def home
   end
