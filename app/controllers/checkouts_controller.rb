dcd60a4e116f4f6ccae4a836e75d35abbc5e3a73
alexandre artus <artusalexandre@yahoo.fr>
2023-11-26 12:02:23 +0100

    retrieve checkout session to success url

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -15,7 +15,7 @@ class CheckoutsController < ApplicationController
         quantity: 1
       }],
       mode: 'payment',
-      success_url: root_url,
+      success_url: root_url + "?session_id={CHECKOUT_SESSION_ID}",
       cancel_url: root_url
     })
     redirect_to @session.url, allow_other_host: true
