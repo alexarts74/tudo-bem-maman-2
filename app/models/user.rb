cfa865e32a7e08ff01f45b07c224c0cd5a7cc3f2
alexandre artus <artusalexandre@yahoo.fr>
2023-11-26 13:04:27 +0100

    user is assign stripe customer id after create

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :clothes, dependent: :destroy
+
+  def to_s
+    email
+  end
+
+  after_create do
+    customer = Stripe::Customer.create(email: email)
+    update(stripe_customer_id: customer.id)
+  end
 end
