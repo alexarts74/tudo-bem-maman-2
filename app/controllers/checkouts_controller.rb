2b71ab66d60569ce359297861e56a8e1d786ea58
alexandre artus <artusalexandre@yahoo.fr>
2023-11-28 15:24:27 +0100

    test heroku for sales-count with the logic in webhook controller

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -16,34 +16,13 @@ class CheckoutsController < ApplicationController
     redirect_to @session.url, allow_other_host: true
   end
 
-  # def create
-  #   clothe = Clothe.find(params[:clothe_id])
-  #   @session = Stripe::Checkout::Session.create({
-  #     customer: current_user.stripe_customer_id,
-  #     payment_method_types: ['card'],
-  #     line_items: [{
-  #       price_data: {
-  #         : clothe.stripe_price_id,
-  #         product_data: {
-  #           name: clothe.name,
-  #         },price
-  #       },
-  #       quantity: 1
-  #     }],
-  #     mode: 'payment',
-  #     success_url: success_url + "?session_id={CHECKOUT_SESSION_ID}",
-  #     cancel_url: cancel_url
-  #   })
-  #   redirect_to @session.url, allow_other_host: true
-  # end
 
   def success
-    session_with_expand = Stripe::Checkout::Session.retrieve({id: params[:session_id], expand: ["line_items"]})
-    session_with_expand.line_items.data.each do |line_item|
-      @clothe = Clothe.find_by(stripe_clothe_id: line_item.price.product)
-      @clothe.update(sales_count: @clothe.sales_count + 1)
-      line_item.price.id
-    end
+    # session_with_expand = Stripe::Checkout::Session.retrieve({id: params[:session_id], expand: ["line_items"]})
+    # session_with_expand.line_items.data.each do |line_item|
+    #   @clothe = Clothe.find_by(stripe_clothe_id: line_item.price.product)
+    #   @clothe.increment!(:sales_count)
+    # end
   end
 
   def cancel
