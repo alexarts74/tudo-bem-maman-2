d1731698439290c9f38ba36238c7cf94772c6a99
alexandre artus <artusalexandre@yahoo.fr>
2023-11-29 18:57:24 +0100

    put the sales count in checkout success controller

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -7,7 +7,7 @@ class CheckoutsController < ApplicationController
       payment_method_types: ['card'],
       line_items: [{
         price: clothe.stripe_price_id,
-        quantity: 1
+        quantity: 1,
       }],
       mode: 'payment',
       success_url: success_url + "?session_id={CHECKOUT_SESSION_ID}",
@@ -18,11 +18,13 @@ class CheckoutsController < ApplicationController
 
 
   def success
-    # session_with_expand = Stripe::Checkout::Session.retrieve({id: params[:session_id], expand: ["line_items"]})
-    # session_with_expand.line_items.data.each do |line_item|
-    #   @clothe = Clothe.find_by(stripe_clothe_id: line_item.price.product)
-    #   @clothe.increment!(:sales_count)
-    # end
+    session_with_expand = Stripe::Checkout::Session.retrieve({id: params[:session_id], expand: ["line_items"]})
+    session_with_expand.line_items.data.each do |line_item|
+      clothe = Clothe.find_by(stripe_clothe_id: line_item.price.product)
+      clothe.increment!(:sales_count)
+    end
+
+    #Voir pour ne pas pouvoir refresh la page et comme ca reglé
   end
 
   def cancel
