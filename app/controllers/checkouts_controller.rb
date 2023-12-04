f47af4de72e7e87c39808b29265ef920f71b09a2
alexandre artus <artusalexandre@yahoo.fr>
2023-12-04 11:11:09 +0100

    multiple checkout

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,31 +1,15 @@
 class CheckoutsController < ApplicationController
 
   def create
-    if Clothe.find(params[:clothe_id])
-      clothe = Clothe.find(params[:clothe_id])
-      @session = Stripe::Checkout::Session.create({
-        customer: current_user.stripe_customer_id,
-        payment_method_types: ['card'],
-        line_items: [{
-          price: clothe.stripe_price_id,
-          quantity: 1,
-        }],
-        mode: 'payment',
-        success_url: success_url + "?session_id={CHECKOUT_SESSION_ID}",
-        cancel_url: cancel_url
-      })
-      redirect_to @session.url, allow_other_host: true
-    else
-      @session = Stripe::Checkout::Session.create({
-        customer: current_user.stripe_customer_id,
-        payment_method_types: ['card'],
-        line_items: @cart.collect { |item| item.to_builder.attributes! },
-        mode: 'payment',
-        success_url: success_url + "?session_id={CHECKOUT_SESSION_ID}",
-        cancel_url: cancel_url
-      })
-      redirect_to @session.url, allow_other_host: true
-    end
+    @session = Stripe::Checkout::Session.create({
+      customer: current_user.stripe_customer_id,
+      payment_method_types: ['card'],
+      line_items: @cart.collect { |item| item.to_builder.attributes! },
+      mode: 'payment',
+      success_url: success_url + "?session_id={CHECKOUT_SESSION_ID}",
+      cancel_url: cancel_url
+    })
+    redirect_to @session.url, allow_other_host: true
   end
 
 
