97f6f164e76b05781784b1fbe1d431e7187d1f0b
alexandre artus <artusalexandre@yahoo.fr>
2024-06-02 22:32:56 +0200

    filter console.log +

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -35,21 +35,3 @@ class CheckoutsController < ApplicationController
   end
 end
 
-
-
-
-# def create
-#   clothe = Clothe.find(params[:clothe_id])
-#   @session = Stripe::Checkout::Session.create({
-#     customer: current_user.stripe_customer_id,
-#     payment_method_types: ['card'],
-#     line_items: [{
-#       price: clothe.stripe_price_id,
-#       quantity: 1,
-#     }],
-#     mode: 'payment',
-#     success_url: success_url + "?session_id={CHECKOUT_SESSION_ID}",
-#     cancel_url: cancel_url
-#   })
-#   redirect_to @session.url, allow_other_host: true
-# end
